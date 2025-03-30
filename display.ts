f0e318ea405d06fed8b16daadeaf71633d7130b9
Pelle Mattsson <pelle.mattsson@lynes.io>
2025-03-30 12:51:27 +0200

    cleanup

--- display.ts
+++
@@ -1,6 +0,0 @@
-import { search } from "./history.ts";
-
-export function display(history: string[], query: string) {
-  const results = search(history, query);
-  results.forEach((x) => console.log(history[x]));
-}
