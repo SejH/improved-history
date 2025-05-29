8931260275e11a020928950da0664d65db043d9b
SejH <pellem.92@gmail.com>
2025-05-29 14:31:00 +0200

    add util to find the unicode of a input
    "deno inputFinderUtil.ts" to run. It will output the unicode string that can be added to the inputHandlers

---
+++ inputFinderUtil.ts
@@ -0,0 +1,25 @@
+async function main() {
+  const input = Deno.stdin;
+  input.setRaw(true);
+
+  const data = new Uint8Array(3);
+  const n = await input.read(data);
+
+  if (!n) {
+    return;
+  }
+
+  console.log(data);
+  let result = "\\u00";
+  data.slice(0, n).forEach((x) => {
+    if (x >= 0x20 && x <= 0x7f) {
+      const char = String.fromCharCode(x);
+      result += char;
+    } else {
+      result += x.toString(16);
+    }
+  });
+  console.log(result);
+}
+
+await main();
